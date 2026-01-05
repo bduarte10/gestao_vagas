e965f57f45a12cf69d0e19d3eb4cb44aee894f10
Bruno Duarte <duartebruno2208@gmail.com>
2026-01-04 22:24:37 -0300

    feat(gestao_vagas): integrate candidate repository to handle persistence operations

--- src/main/java/br/com/duartedev/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/duartedev/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,7 +2,9 @@ package br.com.duartedev.gestao_vagas.modules.candidate.controllers;
 
 
 import br.com.duartedev.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.duartedev.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
-    @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidate) {
-        System.out.println("Candidato Criado: " + candidate);
 
+    private final CandidateRepository candidateRepository;
+
+    @Autowired
+    public CandidateController(CandidateRepository candidateRepository) {
+        this.candidateRepository = candidateRepository;
+    }
+
+    @PostMapping("/")
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidate) {
+       return this.candidateRepository.save(candidate);
     }
 
 }
