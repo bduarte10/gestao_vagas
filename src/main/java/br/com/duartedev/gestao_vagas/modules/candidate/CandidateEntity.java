7a8a5ad24dceb8f11afe19e647ec795f3046f8c4
Bruno Duarte <duartebruno2208@gmail.com>
2026-01-04 22:02:44 -0300

    feat(gestao_vagas): add entity mapping and update database configuration

--- src/main/java/br/com/duartedev/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/duartedev/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,16 +1,26 @@
 package br.com.duartedev.gestao_vagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String name;
@@ -27,5 +37,8 @@ public class CandidateEntity {
 
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
